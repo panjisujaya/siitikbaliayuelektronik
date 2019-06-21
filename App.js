56f09231ae1f6dd736577e106180bab73d65df31
Panji Sujaya <48647339+panjisujaya@users.noreply.github.com>
2019-06-21 22:40:25 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import AppNavigator from './src/AppNavigator';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <AppNavigator />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+});
